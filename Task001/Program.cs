81ca1a4e705c26748d892c16a7728262691d64ab
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-04-15 14:02:15 +0300

    Готовое решение первой задачи(с описанием) в редакции

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -9,7 +9,7 @@
 
 // 8 7,8 -7,1 9
 
-
+Console.Clear();
 int lines = ReadNumberFromConsole("Введите желаемое количество строк");
 int columns = ReadNumberFromConsole("Введите желаемое количество столбцов");
 double[,] numbers = new double[lines, columns];   // объявляю новый массив с данными пользователя
@@ -17,39 +17,38 @@ FillArrayRandomNumbers(numbers);    // команда на заполнение 
 Console.WriteLine("Мы получили массив:");   // для "красоты")
 PrintArray(numbers);  // выводим массив на печать
 
-void FillArrayRandomNumbers(double[,] array)  // метод наполнения двумерного массива случайными числами
+void FillArrayRandomNumbers(double[,] array)  // метод для наполнения двумерного массива случайными числами
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;   // деление на 10 использую для получения дробного результата
-        }
+      array[i, j] = Convert.ToDouble(new Random().Next(-99, 100)) / 10; // деление на 10 использую для получения дробного результата
     }
+  }
 }
 
 void PrintArray(double[,] array)   // метод для вывода массива на печать
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    Console.Write("[  ");
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        Console.Write("[  ");
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]}  ");
-        }
-        Console.Write("]");
-        Console.WriteLine("");
+      Console.Write($"{array[i, j]}  ");
     }
+    Console.Write("]");
+    Console.WriteLine();
+  }
 }
 
 int ReadNumberFromConsole(string message = "")  // метод для чтения из консоли введённого значения
 {
-    if (message != "")
-        Console.WriteLine(message);
-    string input = Console.ReadLine();
-    return int.Parse(input);
+  if (message != "")
+    Console.WriteLine(message);
+  string input = Console.ReadLine();
+  return int.Parse(input);
 }
- 
 
 
 
